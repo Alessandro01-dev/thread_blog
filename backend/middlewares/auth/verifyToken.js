40dfabab2047e708639274ef64509b3f9a51ab80
Alessandro Aronica <ale.aronica@outlook.it>
2026-04-05 23:16:05 +0200

    updated verifyToken with database user validation

--- backend/middlewares/auth/verifyToken.js
+++ backend/middlewares/auth/verifyToken.js
@@ -1,18 +1,28 @@
 const jwt = require('jsonwebtoken')
 const InvalidOrMissingTokenException = require('../../exceptions/auth/InvalidOrMissingTokenException')
+const AuthorSchema = require('../../modules/author/author.schema')
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
 
   const token = req.header('Authorization')
   if (!token) {
-    throw new InvalidOrMissingTokenException()
+    return next(new InvalidOrMissingTokenException())
   }
+
   try {
     const sanitizedToken = token.replace('Bearer ', '')
-    req.user = jwt.verify(sanitizedToken, process.env.JWT_SECRET)
+    const decoded = jwt.verify(sanitizedToken, process.env.JWT_SECRET)
+    
+    const user = await AuthorSchema.findById(decoded.id).select('-password')
+    
+    if (!user) {
+      throw new InvalidOrMissingTokenException()
+    }
+
+    req.user = user
     next()
   } catch (error) {
-    throw new InvalidOrMissingTokenException()
+    next(new InvalidOrMissingTokenException())
   }
 }
 
